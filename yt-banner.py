29379bac556e4dc251142f34c2ac6e4c678c83b2
Loli <89763268+Loli1337@users.noreply.github.com>
2021-08-30 17:00:35 -0700

    Delete yt-banner.py

--- yt-banner.py
+++
@@ -1,4 +0,0 @@
-from sys import argv
-from requests import get
-from re import search
-open('channels4_banner.png', 'wb').write(get(search('https://yt3.ggpht.com/[^=]+=w2560', get(argv[1]).text).group()).content)
